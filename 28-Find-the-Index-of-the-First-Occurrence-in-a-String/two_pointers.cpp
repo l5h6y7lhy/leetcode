7fc555eddb7d93e4924be82f235558c32381b414
Hongyi Li <hongyili@wirelessprv-10-194-227-106.near.illinois.edu>
2024-12-19 21:52:34 -0600

    codes for Problem 28

---
+++ 28-Find-the-Index-of-the-First-Occurrence-in-a-String/two_pointers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int j;
+        int objl = haystack.size();
+        int tarl = needle.size();
+
+        if (objl < tarl) {return -1;}
+
+        for (int i = 0; i <= objl - tarl; i++) {
+            for (j = i; j < i + tarl && j < objl; j++) {
+                if (haystack[j] != needle[j - i]) {break;}
+            }
+
+            if (j - i == tarl) {return i;}
+        }
+
+        return -1;
+    }
+};
