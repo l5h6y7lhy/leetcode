99997c5835d20d15f0d12ec33d62a1fa81541735
Hongyi Li <hongyili@HongyideMacBook-Air.local>
2025-01-05 17:00:03 -0600

    codes for Problem 34

---
+++ 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array/binary_search_based.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        result.push_back(-1);
+        result.push_back(-1);
+
+        if (nums.empty()) {return result;}
+
+        limit = 0;
+        start = 0;
+        end = nums.size() - 1;
+        binarySearch(nums, target);
+
+        limit = 1;
+        start = 0;
+        end = nums.size() - 1;
+        binarySearch(nums, target);
+
+        return result;
+    }
+
+private:
+    vector<int> result;
+    int limit;
+    int start;
+    int end;
+
+    void binarySearch(vector<int>& nums, int& target) {
+        if (end < start) {return;}
+
+        if (start == end) {
+            if (nums[start] == target) {result[limit] = start;}
+            return;
+        }
+
+        int middle = (start + end) / 2;
+
+        if (nums[middle] == target) {
+            result[limit] = middle;
+
+            if (!limit) {end = middle - 1;}
+            else {start = middle + 1;}
+        }
+        else if (nums[middle] >= target) {
+            end = middle - 1;
+        }
+        else {
+            start = middle + 1;
+        }
+
+        binarySearch(nums, target);
+        return;
+    }
+};
