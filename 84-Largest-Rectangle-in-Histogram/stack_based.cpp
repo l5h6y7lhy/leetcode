0aa2088d27a02a7f899197c4545c7e2e165fd995
Hongyi Li <hongyili@wirelessprv-10-194-227-106.near.illinois.edu>
2025-02-20 16:51:22 -0600

    code for Problem 84

---
+++ 84-Largest-Rectangle-in-Histogram/stack_based.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int l = heights.size();
+        stack<int> r;
+        vector<int> result(l, 0);
+
+        for (int i = 0; i < l; i++) {
+            while (!r.empty()) {
+                int curr = r.top();
+
+                if (heights[curr] <= heights[i]) {
+                    break;
+                }
+
+                result[curr] += heights[curr] * (i - curr - 1);
+                r.pop();
+            }
+
+            int lb = r.empty() ? -1 : r.top();
+            result[i] += heights[i] * (i - lb);
+            r.push(i);
+        }
+
+        int rb = r.top();
+        r.pop();
+
+        while (!r.empty()) {
+            int lo = r.top();
+            result[lo] += heights[lo] * (rb - lo);
+            r.pop();
+        }
+        int f = result[0];
+
+        for (int i = 1; i < l; i++) {
+            if (result[i] > f) {f = result[i];}
+        }
+
+        return f;
+    }
+};
