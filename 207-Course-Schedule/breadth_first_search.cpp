e790c0d0b13c121ed53079d442129986e49a259b
Hongyi Li <hongyili@HongyideMacBook-Air.local>
2025-02-28 22:28:29 -0600

    code for Problem 207

---
+++ 207-Course-Schedule/breadth_first_search.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        c = 0;
+        v.assign(numCourses, false);
+        q.assign(numCourses, 0);
+        p.resize(numCourses);
+
+        for (int i = 0; i < prerequisites.size(); i++) {
+            p[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            q[prerequisites[i][0]]++;
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!q[i] && !v[i]) {
+                bfs(i);
+            }
+        }
+
+        return (c == numCourses);
+    }
+
+private:
+    int c;
+    vector<bool> v;
+    vector<int> q;
+    vector<vector<int>> p;
+
+    void bfs(int start) {
+        queue<int> f;
+        f.push(start);
+
+        while (!f.empty()) {
+            int curr = f.front();
+            f.pop();
+            v[curr] = true;
+            c++;
+
+            for (int i = 0; i < p[curr].size(); i++) {
+                if (q[p[curr][i]]) {
+                    q[p[curr][i]]--;
+                }
+
+                if (!q[p[curr][i]]) {
+                    f.push(p[curr][i]);
+                }
+            }
+        }
+
+        return;
+    }
+};
---
+++ 207-Course-Schedule/depth_first_search.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        c = 0;
+        v.assign(numCourses, false);
+        q.assign(numCourses, 0);
+        p.resize(numCourses);
+
+        for (int i = 0; i < prerequisites.size(); i++) {
+            p[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            q[prerequisites[i][0]]++;
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!q[i] && !v[i]) {
+                dfs(i);
+            }
+        }
+
+        return (c == numCourses);
+    }
+
+private:
+    int c;
+    vector<bool> v;
+    vector<int> q;
+    vector<vector<int>> p;
+
+    void dfs(int start) {
+        v[start] = true;
+        c++;
+
+        for (int i = 0; i < p[start].size(); i++) {
+            if (q[p[start][i]]) {
+                q[p[start][i]]--;
+            }
+
+            if (!q[p[start][i]]) {
+                dfs(p[start][i]);
+            }
+        }
+
+        return;
+    }
+};
