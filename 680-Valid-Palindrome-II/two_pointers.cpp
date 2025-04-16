0e313a3c1d754d7625fd9f495143df03d73501c0
Hongyi Li <hongyili@wirelessprv-10-194-227-106.near.illinois.edu>
2025-04-16 09:35:53 -0500

    code for Problem 680

---
+++ 680-Valid-Palindrome-II/two_pointers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+
+        while (i < j) {
+            if (s[i] != s[j]) {break;}
+            i++;
+            j--;
+        }
+
+        if (i == j) {return true;}
+
+        return (PalindromeCheck(s, i + 1, j) || PalindromeCheck(s, i, j - 1));
+    }
+
+private:
+    bool PalindromeCheck(string& s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end]) {return false;}
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
