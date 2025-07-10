1be36a52992931d8991581158f39121b8bc617c7
Hongyi Li <hongyili@HongyideMacBook-Air.local>
2025-07-09 22:07:03 -0500

    renew code for Problem 11

--- 11-Container-With-Most-Water/two_pointers.cpp
+++ 11-Container-With-Most-Water/two_pointers.cpp
@@ -6,7 +6,7 @@ public:
         int result = 0;
 
         while(l < r) {
-            int water = (r - l) * std::min(height[l], height[r]);
+            int water = (r - l) * min(height[l], height[r]);
             if (water > result) {result = water;}
 
             if (height[l] <= height[r]) {l++;}
