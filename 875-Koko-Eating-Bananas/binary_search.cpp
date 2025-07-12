bd893d2456672cee66e0a717915e630ddc59bd4d
Hongyi Li <hongyili@HongyideMacBook-Air.local>
2025-07-11 22:24:35 -0500

    code for Problem 875

---
+++ 875-Koko-Eating-Bananas/binary_search.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int ms = 0;
+
+        for (int i = 0; i < piles.size(); i++) {
+            if (piles[i] > ms) {ms = piles[i];}
+        }
+
+        result = INT_MAX;
+        search(piles, h, 1, ms);
+
+        return result;
+    }
+
+private:
+    int result;
+
+    void search(vector<int>& piles, int& h, int l, int r) {
+        if (r < l) {return;}
+
+        bool exceed = false;
+        int steps = 0;
+        int m = (l + r) / 2;
+
+        for (int i = 0; i < piles.size(); i++) {
+            int remain = 0;
+            if (piles[i] % m != 0) {remain = 1;}
+
+            int cs = (piles[i] / m) + remain;
+
+            if (h - steps >= cs) {steps += cs;}
+            else {
+                exceed = true;
+                break;
+            }
+        }
+
+        if (!exceed) {
+            result = m;
+            search(piles, h, l, m - 1);
+        }
+        else {
+            search(piles, h, m + 1, r);
+        }
+
+        return;
+    }
+};
