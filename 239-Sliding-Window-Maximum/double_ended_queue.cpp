8d26b65f85ca335acfd00b5713381385a4c8f206
Hongyi Li <hongyili@HongyideMacBook-Air.local>
2025-07-13 19:53:14 -0500

    slightly more concise code for Problem 239

--- 239-Sliding-Window-Maximum/double_ended_queue.cpp
+++ 239-Sliding-Window-Maximum/double_ended_queue.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        if (k == 1) {return nums;}
-
         deque<int> q;
         vector<int> result;
 
         for (int i = 0; i < nums.size(); i++) {
-            while (!q.empty() && i - q.front() > k - 1) {q.pop_front();}
+            if (!q.empty() && i - q.front() > k - 1) {q.pop_front();}
 
             while (!q.empty() && nums[i] >= nums[q.back()]) {
                 q.pop_back();
