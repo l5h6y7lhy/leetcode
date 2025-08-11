459f655d086597fb11ba49990b5994b4642ec0ba
Hongyi Li <hongyili@HongyideMacBook-Air.local>
2025-08-10 21:03:05 -0500

    slightly more precise code for Problem 2330

--- 2330-Valid-Palindrome-IV/two_pointers.cpp
+++ 2330-Valid-Palindrome-IV/two_pointers.cpp
@@ -6,7 +6,7 @@ public:
 
         while (i < j) {
             if (s[i] != s[j]) {
-                if (c >= 2) {break;}
+                if (c == 2) {break;}
                 c++;
             }
             i++;
